ce227af62769f847f32abe477cbea0cce3bdd416
phanThinh4 <tp041199@gmail.com>
2024-07-21 14:56:56 +0700

    danhMucCtrl

--- src/main/java/com/poly/sms/config/AuthenticationSuccessHandler.java
+++ src/main/java/com/poly/sms/config/AuthenticationSuccessHandler.java
@@ -1,19 +1,19 @@
-package com.poly.sms.config;
+// package com.poly.sms.config;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+// import org.springframework.security.core.Authentication;
+// import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.ServletException;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
 
-public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) throws ServletException, IOException {
+// public class AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+//     @Override
+//     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+//             Authentication authentication) throws ServletException, IOException {
 
-        response.sendRedirect("/sms/home");
-    }
-}
+//         response.sendRedirect("/sms/home");
+//     }
+// }
