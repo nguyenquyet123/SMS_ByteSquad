9db0f3690dea59678461e94a4d9c22c9944e6fbd
phanThinh4 <tp041199@gmail.com>
2024-07-21 14:56:56 +0700

    danhMucCtrl

--- src/main/java/com/poly/sms/controller/app/EmployeeController.java
+++ src/main/java/com/poly/sms/controller/app/EmployeeController.java
@@ -5,7 +5,7 @@ import com.poly.sms.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +17,8 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    // @Autowired
+    // private PasswordEncoder passwordEncoder;
 
     @GetMapping
     public ResponseEntity<List<Employee>> getAllEmployees() {
@@ -35,7 +35,7 @@ public class EmployeeController {
 
     @PostMapping
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
-        employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+        // employee.setPassword(passwordEncoder.encode(employee.getPassword()));
         Employee savedEmployee = employeeService.save(employee);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
@@ -46,7 +46,7 @@ public class EmployeeController {
         if (!username.equals(employee.getUsername())) {
             return ResponseEntity.badRequest().build();
         }
-        employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+        // employee.setPassword(passwordEncoder.encode(employee.getPassword()));
         Employee updatedEmployee = employeeService.save(employee);
         return ResponseEntity.ok(updatedEmployee);
     }
