ce4fcbda38f1ed2c3daac16f4d23f033ef2a3aea
ThanhToanDev-itachi <hoangthanhtuyen.ksmy@gmail.com>
2024-07-22 13:38:14 +0700

    update sendEmail

---
+++ src/main/java/com/poly/sms/service/EmailService.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package com.poly.sms.service;
+
+import com.poly.sms.entity.Email;
+
+/**
+ *
+ * @author hoang
+ */
+public interface EmailService {
+  
+
+    void sendEmail(Email email);
+}
+
